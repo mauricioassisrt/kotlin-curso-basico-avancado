447b6e50c6f83dd8188a8b11fec433995452d990
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-17 15:38:16 -0300

    inicial

--- src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
@@ -4,4 +4,6 @@ import com.mercadolivro.model.CustomerModel
 import org.springframework.data.repository.CrudRepository
 
 interface CustomerRepository: CrudRepository<CustomerModel, Int> {
+
+    fun findByNameContaining(name: String): List<CustomerModel>
 }
--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -7,33 +7,33 @@ import org.springframework.stereotype.Service
 @Service
 class CustomerService(val customerRepository: CustomerRepository) {
 
-    val customers = mutableListOf<CustomerModel>()
-
     fun getAll(name: String?): List<CustomerModel> {
         name?.let {
-            return customers.filter { it.name!!.contains(name, true) }
+            return customerRepository.findByNameContaining(name)
         }
-        return customers
+        return  customerRepository.findAll().toList()
     }
 
     fun create(customer: CustomerModel) {
         customerRepository.save(customer)
-
     }
 
     fun getCustomer(id: Int): CustomerModel {
-        return customers.filter { it.id == id }.first()
+        return customerRepository.findById(id).orElseThrow()
     }
 
     fun update(customer: CustomerModel) {
-        customers.filter { it.id == customer.id }.first().let {
-            it.name = customer.name
-            it.email = customer.email
-        }
+        if (!customerRepository.existsById(customer.id!!))
+            throw Exception()
+
+        customerRepository.save(customer)
+
     }
 
     fun delete(id: Int) {
-        customers.removeIf { it.id == id }
+        if (!customerRepository.existsById(id))
+            throw Exception()
+        customerRepository.deleteById(id)
     }
 
 }
