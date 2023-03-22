fc52c49989a0d81cea2e13975845ed31db7a0854
Mauricio Assis <mauricio.nextage@gmail.com>
2023-03-22 16:29:29 -0300

    93

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -67,7 +67,7 @@ class SecurityConfig(
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.addAllowedOrigin("*")
+        config.addAllowedOriginPattern("*")
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
         source.registerCorsConfiguration("/**", config)
--- src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
@@ -11,6 +12,7 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@Profile("!prod")
 @EnableSwagger2
 @Configuration
 class SwaggerConfig {
